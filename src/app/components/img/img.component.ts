91d143216a96543cbe30a387af3d23e69d3fcda8
christian <cristhho@gmail.com>
2023-06-10 21:31:48 -0500

    components, Input decorator

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent {
+  @Input() img: string = 'https://www.w3schools.com/howto/img_avatar.png'
+}
