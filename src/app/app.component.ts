c8e26affb01075b099c61a38eb8e0d3538149c14
christian <cristhho@gmail.com>
2023-06-11 00:08:46 -0500

    products grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model';
 
 
 //https://www.w3schools.com/howto/img_avatar.png
@@ -10,33 +9,6 @@ import { Product } from './models/product.model';
 })
 export class AppComponent {
   imgInput = '';
-
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      img: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      img: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      img: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      img: './assets/images/books.jpg'
-    }
-  ]
 
   onLoaded(event: string) {
     console.log('From parent', event)
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent {
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      img: './assets/images/toy.jpg'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      img: './assets/images/bike.jpg'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      img: './assets/images/album.jpg'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      img: './assets/images/books.jpg'
+    }
+  ]
+}
