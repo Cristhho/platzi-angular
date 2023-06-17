26256898989077538c37865eb62bf809edcf7f62
christian <cristhho@gmail.com>
2023-06-17 15:51:11 -0500

    website module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './website/pages/home/home.component';
 import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
-import { LayoutComponent } from './website/components/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'category/:id',
-        component: CategoryComponent
-      },
-      {
-        path: 'product/:id',
-        component: ProductDetailComponent
-      }
-    ]
+    loadChildren: () => import('./website/website.module').then((module) => module.WebsiteModule)
   },
   {
     path: 'admin',
