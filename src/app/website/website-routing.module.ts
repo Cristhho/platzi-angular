fb2b634fc4739d1b539a9f9be6762d1cecfaf1da
christian <cristhho@gmail.com>
2023-06-17 16:28:29 -0500

    category module and lazy loading

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
@@ -21,8 +20,8 @@ const routes: Routes = [
         component: HomeComponent
       },
       {
-        path: 'category/:id',
-        component: CategoryComponent
+        path: 'category',
+        loadChildren: () => import('./pages/category/category.module').then((module) => module.CategoryModule)
       },
       {
         path: 'product/:id',
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -5,7 +5,7 @@ import { register } from 'swiper/element/bundle';
 import { WebsiteRoutingModule } from './website-routing.module';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
+
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -21,7 +21,6 @@ register()
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     MyCartComponent,
     LoginComponent,
     RegisterComponent,
