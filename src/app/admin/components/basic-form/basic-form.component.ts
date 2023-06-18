9df6acc2631a0decfd1b25e7e9287a77a8c6a733
christian <cristhho@gmail.com>
2023-06-18 02:03:31 -0500

    form group

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,28 +8,62 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('', [Validators.required, Validators.maxLength(20)]);
-  emailField = new FormControl('');
-  phoneField = new FormControl('');
-  colorField = new FormControl('#000000');
-  dateField = new FormControl('');
-  ageField = new FormControl(12);
-
-  categoryField = new FormControl('cat_1')
-  tagsField = new FormControl('')
-  agreementField = new FormControl(false)
-  genderField = new FormControl()
-  zoneField = new FormControl()
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    color: new FormControl('#000000'),
+    date: new FormControl(''),
+    age: new FormControl(12),
+    category: new FormControl('cat_1'),
+    tags: new FormControl(''),
+    agreement: new FormControl(false),
+    gender: new FormControl(),
+    zone: new FormControl()
+  })
+  get nameField() {
+    return this.form.get('name')
+  }
+  get emailField() {
+    return this.form.get('email')
+  }
+  get phoneField() {
+    return this.form.get('phone')
+  }
+  get colorField() {
+    return this.form.get('color')
+  }
+  get dateField() {
+    return this.form.get('date')
+  }
+  get ageField() {
+    return this.form.get('age')
+  }
+  get categoryField() {
+    return this.form.get('category')
+  }
+  get tagsField() {
+    return this.form.get('tags')
+  }
+  get agreementField() {
+    return this.form.get('agreement')
+  }
+  get genderField() {
+    return this.form.get('gender')
+  }
+  get zoneField() {
+    return this.form.get('zone')
+  }
 
   ngOnInit(): void {
-    this.nameField.valueChanges
+    this.nameField?.valueChanges
     .subscribe(value => {
       console.log(value);
     });
   }
 
   getNameValue() {
-    console.log(this.nameField.value);
+    console.log(this.nameField?.value);
   }
 
 }
