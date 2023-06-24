a5e11005bc7adf10afd402e96e8310e692881e19
christian <cristhho@gmail.com>
2023-06-24 18:23:05 -0500

    update pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-forms';
+
+  constructor(
+    private update: SwUpdate
+  ){}
+
+  ngOnInit(): void {
+      this.updatePWA()
+  }
+
+  updatePWA() {
+    this.update.versionUpdates.subscribe((event) => {
+      if (event.type === 'VERSION_READY') {
+        window.location.reload()
+      }
+    })
+  }
 }
