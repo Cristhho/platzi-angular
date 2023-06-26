4b3dbae4b95b0cf2d0a84a8408aaf287a3480389
christian <cristhho@gmail.com>
2023-06-25 19:10:53 -0500

    unsubscribe in ngOnDestroy

--- src/app/contact/components/layout/layout.component.ts
+++ src/app/contact/components/layout/layout.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { EmployeeData } from '../../../core/models/employee.model';
 import { GeneratorService } from '../../../core/services/generator.service';
+import { Subscription } from 'rxjs';
 
 const names = ['nicolas', 'juan', 'felipe', 'maria'];
 
@@ -10,9 +11,13 @@ const names = ['nicolas', 'juan', 'felipe', 'maria'];
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
   salesList: EmployeeData[] = [];
   bList: EmployeeData[] = [];
+
+  value = 0
+
+  private sub$!: Subscription
 
   constructor(
     private generatorService: GeneratorService
@@ -21,6 +26,16 @@ export class LayoutComponent implements OnInit {
   ngOnInit() {
     this.salesList = this.generatorService.generate(names, [10, 20], 10);
     this.bList = this.generatorService.generate(names, [10, 20], 10);
+
+    this.sub$ = this.generatorService.getData()
+      .subscribe((val) => {
+        console.log(val)
+        this.value = val
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe()
   }
 
   addItem(list: EmployeeData[], label: string) {
