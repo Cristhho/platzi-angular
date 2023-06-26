d42b5449684294455d7d6388648f6859cbb08d08
christian <cristhho@gmail.com>
2023-06-26 00:12:36 -0500

    test products service

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { ProductsService } from './products.service';
 
-describe('ProductsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('ProductsService', () => {
+  let service: ProductsService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    TestBed.configureTestingModule({
+      providers: [
+        ProductsService,
+        {
+          provide: HttpClient, useValue: httpClientSpy
+        }
+      ]
+    });
+    service = TestBed.inject(ProductsService);
+  });
 
   it('should be created', () => {
-    const service: ProductsService = TestBed.get(ProductsService);
     expect(service).toBeTruthy();
   });
 });
