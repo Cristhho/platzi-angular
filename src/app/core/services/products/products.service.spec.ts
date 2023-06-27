ba6cd8a1ec32ab8afe9c074fb382e2c63f64b486
christian <cristhho@gmail.com>
2023-06-26 21:08:51 -0500

    HttpClientTestingModule

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,26 +1,36 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ProductsService } from './products.service';
+import { environment } from '../../../../environments/environment';
 
 fdescribe('ProductsService', () => {
   let service: ProductsService;
-  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
       providers: [
-        ProductsService,
-        {
-          provide: HttpClient, useValue: httpClientSpy
-        }
+        ProductsService
       ]
     });
     service = TestBed.inject(ProductsService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call once', (done: DoneFn) => {
+
+    service.getAllProducts().subscribe({
+      next: () => {done()}
+    })
+
+    const req = httpController.expectOne(`${environment.url_api}/products`)
+    req.flush([])
+    httpController.verify()
   });
 });
