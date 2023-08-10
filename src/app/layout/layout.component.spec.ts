1daa9b1798665a95b16a9065461092f65ba8da53
christian <cristhho@gmail.com>
2023-08-09 22:53:47 -0500

    test nested components

--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,17 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LayoutComponent } from './layout.component';
 
-xdescribe('LayoutComponent', () => {
+@Component({selector: 'app-header', template: ''})
+class HeaderStubComponent {
+}
+
+@Component({selector: 'app-footer', template: ''})
+class FooterStubComponent {
+}
+
+describe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        LayoutComponent,
+        HeaderStubComponent,
+        FooterStubComponent
+      ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LayoutComponent);
